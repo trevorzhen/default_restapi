8d32f976becbd8ce1ea78c175484b9f59f87cc74
trevorzhen <trevorzhen@gmail.com>
2021-09-01 22:09:53 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import pickle
+import numpy as np
+import pandas as pd
+
+APP = Flask(__name__)
+API = Api(APP)
+
+
+model = pickle.load(open('model_train', 'rb'))
+data_prep= pickle.load(open("data_transformer", 'rb'))
+processed_data = pd.read_csv("defaults_selected.csv")
+data_prep.fit(processed_data)
+
+class Predict(Resource):
+
+    @staticmethod
+    def post():
+        # args = request.json
+
+        instance = reqparse.RequestParser()
+        instance.add_argument('Credit')
+        instance.add_argument('Gender')
+        instance.add_argument('Education')
+        instance.add_argument('Marital') # Number of Children (0-5)
+        instance.add_argument('Age')     # Yes = 1 or No = 0
+        instance.add_argument('Default')   # Options: 1,2,3 or 4
+        args = instance.parse_args()       # creates dict
+
+        input_df = pd.DataFrame([args])
+        x = data_prep.transform(input_df)
+
+        out = {'Prediction': np.where(model.predict([x[0,0:-1]]) > 0.5, "Yes", "No")[0], 'x_new':args}
+
+        return out, 200
+
+API.add_resource(Predict, '/predict')
+
+if __name__ == '__main__':
+    APP.run(debug=True, port='1080')
